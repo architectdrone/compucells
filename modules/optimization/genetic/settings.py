927cfeacb7147ea9a3f3565cb9656f028e347c54
Owen Mellema <owen.mellema@hyland.com>
2019-08-12 14:16:08 -0500

    Remove settings.py

--- modules/optimization/genetic/settings.py
+++
@@ -1,8 +0,0 @@
-FUNCTION_NAME = "ADD1"
-CC_PER_BATCH = 10 #Number of compucells per batch
-FUNCTION_SPACE_SIZE = 3 #Number of columns in the function space
-ITERATIONS = FUNCTION_SPACE_SIZE
-RUNS = 300 #Number of times to run the simulation
-PERFORMANCE_THRESHOLD = 0.95 #If a compucell performs better than this threshold, stop the simulation and return
-INPUT_SPACE_SIZE = 4
-MUTATION_CHANCE = 30/((FUNCTION_SPACE_SIZE*INPUT_SPACE_SIZE)+512)#30 -> 80%
